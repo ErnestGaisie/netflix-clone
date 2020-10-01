05d27f011c18c8f407cb53e0e938665a8c074840
Ernest Gaisie <ernestyawgaisie@gmail.com>
2020-10-01 11:54:14 +0000

    Updates

---
+++ precache-manifest.c5822a90a330d23b82c695aedf5deb68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b7428f192e0046cecb1185c49b3468",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "5f47e8cf7e5e5c8bc048",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.06649b95.chunk.css"
+  },
+  {
+    "revision": "1ffed8eb19fa4c482698",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.1d09a038.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.1d09a038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f47e8cf7e5e5c8bc048",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.b49206e0.chunk.js"
+  },
+  {
+    "revision": "585d81325aa2e16a8120",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.62633f51.js"
+  }
+]);
